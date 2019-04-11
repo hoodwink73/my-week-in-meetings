43161578e80d67500d62863daa45fe52b745f450
Arijit Bhattacharya <hoodwink73@live.com>
2019-04-11 17:49:31 +0530

    Support theme for all stories
    Decorate all stories with the ThemeProvider from emotion-theming

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,17 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { ThemeProvider } from "emotion-theming";
+
+import theme from "../src/theme";
+
+const ThemeDecorator = storyFn => (
+  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+);
+
+addDecorator(ThemeDecorator);
 
 function loadStories() {
-  require('../src/stories');
+  require("../src/stories");
 }
 
 configure(loadStories, module);
