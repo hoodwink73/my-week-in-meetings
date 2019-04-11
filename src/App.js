03f0eed846871a1068ed1ac9cc29a8146f86dbaa
Arijit Bhattacharya <hoodwink73@live.com>
2019-04-11 17:04:23 +0530

    Add a style and theme system

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ThemeProvider } from "emotion-theming";
+import { Box } from "@rebass/emotion";
 
-class App extends Component {
-  render() {
-    return (
+import theme from "./theme";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Box color="white" bg="blue">
+          Hello
+        </Box>
       </div>
-    );
-  }
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    blue: "blue",
+    lightgray: "#f6f6ff"
+  },
+  fonts: {
+    sans: "system-ui, sans-serif",
+    mono: "Menlo, monospace"
+  },
+  shadows: {
+    small: "0 0 4px rgba(0, 0, 0, .125)",
+    large: "0 0 24px rgba(0, 0, 0, .125)"
+  }
+};
