350f12d06d91cc7c97dd3910af9460d949887f10
Arijit Bhattacharya <hoodwink73@live.com>
2019-05-16 15:16:15 +0530

    Sync the timer to fire at the change of a real minute

--- src/hooks/useRerender.js
+++ src/hooks/useRerender.js
@@ -1,14 +1,22 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 export default function(intervalInMs) {
   const [renderCounter, setRenderCounter] = useState(0);
+  const secondsInNow = moment().seconds();
+
+  // this tries to ensure we trigger the update when the minute changes
+  // in real world
+  // NOTE: If you want updates to happen after duration less than a minute
+  // this code will not work
+  const scheduleTime = (intervalInMs / 1000 - secondsInNow) * 1000;
 
   useEffect(() => {
-    const unsubscribeID = setInterval(() => {
+    const unsubscribeID = setTimeout(() => {
       setRenderCounter(renderCounter + 1);
-    }, intervalInMs);
+    }, scheduleTime);
 
-    return () => clearInterval(unsubscribeID);
+    return () => clearTimeout(unsubscribeID);
   });
 
   return renderCounter;
