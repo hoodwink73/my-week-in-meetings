f9b49a1fde53d6698c4c401453744b04a18ec4fe
Arijit Bhattacharya <hoodwink73@live.com>
2019-06-28 18:21:14 +0530

    Add animations for our errors

--- src/components/Errors/Errors.js
+++ src/components/Errors/Errors.js
@@ -2,6 +2,7 @@ import React, { useRef, useEffect, useContext } from "react";
 import { createPortal } from "react-dom";
 import Alert from "@reach/alert";
 import { Flex, Box, Card, Text } from "@rebass/emotion";
+import { useTransition, animated, config } from "react-spring";
 
 import {
   ErrorManagerContext,
@@ -23,44 +24,62 @@ export default function Errors() {
     };
   }, []);
 
-  if (portalParentRef.current) {
-    return createPortal(
-      <>
-        {errorsState.map(error => (
-          <Card
-            bg="red.0"
-            key={error.id}
-            mb={3}
-            borderRadius={4}
-            p={2}
-            boxShadow="small"
-          >
-            <Alert>
-              <Flex>
-                <Text
-                  color="red.2"
-                  fontSize={1}
-                  fontWeight="bold"
-                  flex="1 0 auto"
-                >
-                  {error.message}
-                </Text>
-                {error.canDismiss && (
-                  <Box
-                    alignSelf="flex-end"
-                    onClick={() => dispatch({ type: "DISMISS", id: error.id })}
+  const transitions = useTransition(errorsState, error => error.id, {
+    from: { transform: "translate3d(0,100%,0)", opacity: 0 },
+    enter: { transform: "translate3d(0,0px,0)", opacity: 1 },
+    leave: { opacity: 0 }
+  });
+
+  return (
+    <>
+      {portalParentRef.current
+        ? createPortal(
+            <>
+              {transitions.map(({ item, props, key }) => (
+                <animated.div key={key} style={props}>
+                  <Card
+                    as={animated.div}
+                    bg="red.0"
+                    mb={3}
+                    borderRadius={4}
+                    p={3}
+                    boxShadow="small"
                   >
-                    <CloseIcon width="20" style={{ cursor: "pointer" }} />
-                  </Box>
-                )}
-              </Flex>
-            </Alert>
-          </Card>
-        ))}
-      </>,
-      portalParentRef.current
-    );
-  } else {
-    return null;
-  }
+                    <Alert>
+                      <Flex>
+                        <Text
+                          color="red.2"
+                          fontSize={1}
+                          fontWeight="bold"
+                          flex="1 0 auto"
+                        >
+                          {item.message}
+                        </Text>
+                        {item.canDismiss && (
+                          <Box
+                            alignSelf="flex-end"
+                            onClick={() =>
+                              dispatch({
+                                type: "DISMISS",
+                                id: item.id
+                              })
+                            }
+                          >
+                            <CloseIcon
+                              width="20"
+                              style={{ cursor: "pointer" }}
+                            />
+                          </Box>
+                        )}
+                      </Flex>
+                    </Alert>
+                  </Card>
+                </animated.div>
+              ))}
+            </>,
+            portalParentRef.current
+          )
+        : null}
+    </>
+  );
 }
