538947f015f98f0d94c9563d8fb17215e2054a02
Arijit Bhattacharya <hoodwink73@live.com>
2019-07-06 15:51:25 +0530

    Add current time to Greeting

--- src/components/Today/Today.js
+++ src/components/Today/Today.js
@@ -2,5 +2,5 @@ import React from "react";
 import moment from "moment";
 
 export default function Today() {
-  return moment().format("dddd, D MMM");
+  return moment().format("dddd, D MMM, h:mm A");
 }
