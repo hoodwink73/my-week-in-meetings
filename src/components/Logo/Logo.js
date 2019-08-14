75a9834de944b6b9201e0ce9d7e46cfe1ffd44de
Arijit Bhattacharya <hoodwink73@live.com>
2019-08-14 10:11:55 +0530

    Add product hunt badge for larger websites

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 import React from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import { Text } from "@rebass/emotion";
+import { Box, Text } from "@rebass/emotion";
 import useMedia from "react-use/lib/useMedia";
 
 export default function Logo() {
@@ -9,8 +9,9 @@ export default function Logo() {
   const isLargeDesktop = useMedia("(min-width: 90em)");
 
   return (
-    <Text
-      css={css`
+    <>
+      <Text
+        css={css`
       position: absolute;
       text-transform: uppercase;
       top: 20px;
@@ -38,10 +39,27 @@ export default function Logo() {
         `}
   }
     `}
-      fontWeight={700}
-      fontSize={[2, 3]}
-    >
-      Deepwork.today
-    </Text>
+        fontWeight={700}
+        fontSize={[2, 3]}
+      >
+        Deepwork.today
+        {isLarge && (
+          <Box mt={3}>
+            <a
+              href="https://www.producthunt.com/posts/deepwork-today?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-deepwork-today"
+              target="_blank"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=164154&theme=light&period=daily"
+                alt="Deepwork.today - Keep a tab on your day | Product Hunt Embed"
+                style={{ width: "250px", height: "54px" }}
+                width="250px"
+                height="54px"
+              />
+            </a>
+          </Box>
+        )}
+      </Text>
+    </>
   );
 }
